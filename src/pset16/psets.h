a001cbd838d0711e8d73e6299b28f099028620c9
Serge Chernov <gres.cher@icloud.com>
2019-08-23 18:47:12 +0300

    ex16_01 -> create 'psets.h'

---
+++ src/pset16/psets.h
@@ -0,0 +1,16 @@
+/*
+ *  psets.h
+ *  A header file of preprocessor definitions (exercise 1 of the Problem Set 16)
+ *  Created by gres.cher on 08/21/19.
+ */
+
+#ifndef PSETS_H
+#define PSETS_H
+
+#include <stdio.h>
+
+#define EATLINE()   while(getchar() != '\n') continue
+#define MAX(X,Y)    ((X) > (Y) ? (X) : (Y))
+#define MIN(X,Y)    ((X) < (Y) ? (X) : (Y))
+
+#endif /* PSETS_H */

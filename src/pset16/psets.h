c7be324f76212d96701b8c70696f44609e32f2db
Serge Chernov <gres.cher@icloud.com>
2019-08-24 02:20:00 +0300

    psets.h -> add macro for ex16_03.c

--- src/pset16/psets.h
+++ src/pset16/psets.h
@@ -7,18 +7,21 @@
 #ifndef PSETS_H
 #define PSETS_H
 
+#include <math.h>
 #include <stdio.h>
 
 // Skip the remaining input
-#define EATLINE()   while(getchar() != '\n') continue
+#define EATLINE()       while(getchar() != '\n') continue
 
-// The harmonic mean of two numbers (for ex16_02.c)
-#define HRMEAN(X,Y) (2.0 / (1.0/(X) + 1.0/(Y)))
+// The harmonic mean of two numbers (used in ex16_02.c)
+#define HRMEAN(X,Y)     (2.0 / (1.0/(X) + 1.0/(Y)))
 
-// The maximum of two values
-#define MAX(X,Y)    ((X) > (Y) ? (X) : (Y))
+// Conversions between radians and degrees (used in ex16_03.c)
+#define DEG_TO_RAD(X)   ((X) * 4.0 * atan(1) / 180.0)
+#define RAD_TO_DEG(X)   ((X) * 180.0 / (4.0 * atan(1)))
 
-// The minimum of two values
-#define MIN(X,Y)    ((X) < (Y) ? (X) : (Y))
+// The maximum and minimum of two values
+#define MAX(X,Y)        ((X) > (Y) ? (X) : (Y))
+#define MIN(X,Y)        ((X) < (Y) ? (X) : (Y))
 
 #endif /* PSETS_H */

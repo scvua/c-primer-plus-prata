a1d3c78a1d31983893dfc0468db6df58f363178c
Serge Chernov <gres.cher@icloud.com>
2019-08-23 20:49:46 +0300

    psets.h -> add macro for ex16_02.c

--- src/pset16/psets.h
+++ src/pset16/psets.h
@@ -9,8 +9,16 @@
 
 #include <stdio.h>
 
+// Skip the remaining input
 #define EATLINE()   while(getchar() != '\n') continue
+
+// The harmonic mean of two numbers (for ex16_02.c)
+#define HRMEAN(X,Y) (2.0 / (1.0/(X) + 1.0/(Y)))
+
+// The maximum of two values
 #define MAX(X,Y)    ((X) > (Y) ? (X) : (Y))
+
+// The minimum of two values
 #define MIN(X,Y)    ((X) < (Y) ? (X) : (Y))
 
 #endif /* PSETS_H */
